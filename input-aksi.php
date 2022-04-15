384a79dc2addb08e5250360ccb0f2707337de6ea
Zaenil Muttakin <85423601+Zaenil@users.noreply.github.com>
2022-04-16 03:34:26 +0700

    Update input-aksi.php

--- input-aksi.php
+++ input-aksi.php
@@ -1,1 +1,12 @@
+<?php
+    include 'koneksi.php';
+    $kat    = $_POST['kategori'];
+    $nama   = $_POST['nama'];
+    $jual   = $_POST['beli'];
+    $beli   = $_POST['jual'];
+    $stok   = $_POST['stok'];
 
+    mysql_query("INSERT INTO barang VALUES('','$kat','$nama','$jual','$beli','$stok')");
+
+    header("location:index.php?pesan=input");
+?>
