5a6e40de2be210eb78d0dd0ef3b83765ef582063
Zaenil Muttakin <85423601+Zaenil@users.noreply.github.com>
2022-04-16 03:38:12 +0700

    Update update.php

--- update.php
+++ update.php
@@ -1,1 +1,13 @@
+<?php
+    include 'koneksi.php';
+    $id     = $_POST['id'];
+    $kat    = $_POST['kategori'];
+    $nama   = $_POST['nama'];
+    $jual   = $_POST['beli'];
+    $beli   = $_POST['jual'];
+    $stok   = $_POST['stok'];
 
+    mysql_query("UPDATE barang SET kategori='$kat', nama='$nama', hrg_beli='$beli', hrg_jual='$jual', stok='$stok' WHERE id='$id'");
+
+    header("location:index.php?pesan=update");
+?>
